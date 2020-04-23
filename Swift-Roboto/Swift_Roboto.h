8d799af14d2a4e5b313cc81ffdb1118f070f2328
Bobby Rohweder <bobbyrohweder@gmail.com>
2020-04-22 18:06:08 -0700

    Initial Commit

---
+++ Swift-Roboto/Swift_Roboto.h
@@ -0,0 +1,19 @@
+//
+//  Swift_Roboto.h
+//  Swift-Roboto
+//
+//  Created by Bobby Rohweder on 4/22/20.
+//  Copyright © 2020 Bobby Rohweder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swift_Roboto.
+FOUNDATION_EXPORT double Swift_RobotoVersionNumber;
+
+//! Project version string for Swift_Roboto.
+FOUNDATION_EXPORT const unsigned char Swift_RobotoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Roboto/PublicHeader.h>
+
+
